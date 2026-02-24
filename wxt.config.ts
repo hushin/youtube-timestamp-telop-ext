9472cc70d4a9236177d5c7f02b7838db21d5239b
hushin <ngtv.hsn@gmail.com>
2026-02-24 21:47:55 +0900

    feat: @wxt-dev/auto-iconsを追加し、アイコン機能を強化 fix: 不要なアイコンファイルを削除

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'wxt';
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   srcDir: 'src',
-  modules: ['@wxt-dev/module-svelte'],
+  modules: ['@wxt-dev/module-svelte', '@wxt-dev/auto-icons'],
   manifest: {
     name: 'YouTube Timestamp Telop - タイムスタンプコメント',
     version: '0.1.0',
